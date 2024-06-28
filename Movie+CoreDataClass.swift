7e831b800c3ac511e8bd88ef64cce7f612c702da
buhleradzilani <radzilanibuhle@gmail.com>
2024-06-28 13:25:33 +0200

    using the wrapped title variable to handle the null values

---
+++ Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Buhle Radzilani on 2024/06/28.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
